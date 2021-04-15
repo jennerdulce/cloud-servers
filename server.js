57d13123c37f89c51f7aa9d027c8e3466db96e08
Jenner Dulce <jennerdulce@gmail.com>
2021-04-14 21:21:14 -0700

    changed

---
+++ server.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+let data = {};
+
+app.get('/', (req, res) => {
+  res.status(200).send('Changed the reading message!')
+})
+
+app.post('/data', (req, res) => {
+  data = req.body;
+  res.status(200).send(data)
+})
+
+app.listen(PORT, () => {
+  console.log(`Connected to PORT: ${PORT}`)
+})
